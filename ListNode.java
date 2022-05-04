fe671d30d61922dc0b4bfcf31d7ff224adebcf31
christina <103866591+carbonatedchristi@users.noreply.github.com>
2022-05-03 17:10:41 -0700

    Add files via upload

---
+++ ListNode.java
@@ -0,0 +1,5 @@
+public class ListNode {
+
+
+    // node should contain at least name, address, city, and phone number fields in the phonebook node
+}
---
+++ PhonebookManager.java
@@ -0,0 +1,10 @@
+public class PhonebookManager {
+
+    //manager will allow you to either add an entry
+    //(name, address, city, phone #) to the end and then sort it,
+    //or alphabetically by last name.  You can also modify the entry,
+    //delete entries, or move them from the Bellingham to the Seattle
+    //phone book (optional).  Finally, you should be able to print out
+    //your list in a nice format.
+
+}

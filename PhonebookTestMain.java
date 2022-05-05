c6f1726bba8c1524ec575805f471f233d22ba93f
christina <mymrinchristina@gmail.com>
2022-05-04 22:22:57 -0700

    test method --> create methods like transferTo, editName, etc. in pbmanager class --> can edit and mess around with this, keep eye out on 16 and 17, looks weird to me? :?

--- PhonebookTestMain.java
+++ PhonebookTestMain.java
@@ -1,2 +1,21 @@
 public class PhonebookTestMain {
+
+    public static void main(String[] args) {
+        PBManager bellingham = new PBManager("Bellingham");
+        PBManager seattle = new PBManager("Seattle");
+
+        bellingham.add(new PBNode("David", "1337 Weezer St.", "Bellingham", 6882442020));
+
+        seattle.add(new PBNode("Rebecca", "222 Weezer St.", "Seattle", 2026208800));
+
+        System.out.println("I ---------");
+        System.out.println();
+        System.out.println(bellingham);
+        System.out.println(seattle);
+
+        bellingham.transferTo(seattle, bellingham.get("David"));
+        bellingham.transferTo(seattle, bellingham.get("Rebecca"));
+
+        seattle.editName(seattle.get("David"), "Zach"); //should replace davids name with zach
+    }
 }

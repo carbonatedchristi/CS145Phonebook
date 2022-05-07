f5dfb7631d9f5761f828e767cbdd2e247cd91073
Hannah Hendrickson <hhendrickson8440@student.whatcom.edu>
2022-05-06 19:07:41 -0700

    Test commit in ListNode

--- PhoneBook/ListNode.java
+++ PhoneBook/ListNode.java
@@ -6,7 +6,7 @@
 // TODO all classes need to be in a package to properly use protected keyword
 
 package PhoneBook;
-
+// This is a test
 
     public class ListNode {
         // node should contain at least name, address, city, and phone number fields in the phonebook node
